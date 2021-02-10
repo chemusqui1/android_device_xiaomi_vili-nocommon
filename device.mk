91efc3d9495cfe429178920219ef6046fd5b2ae8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:01:44 +0100

    vili: Build health HIDL
    Change-Id: I89c431c7ac87fe878537375cd811606d68f4bb1d

--- device.mk
+++ device.mk
@@ -217,6 +217,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
